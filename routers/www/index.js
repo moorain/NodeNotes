cf8b1c9db92eeb3f67c35b316a1c243698e0d4c4
Moorain <1062254476@qq.com>
2019-06-29 23:37:39 +0800

    Update index.js

--- routers/www/index.js
+++ routers/www/index.js
@@ -2,6 +2,6 @@ const Router=require('koa-router');
 
 let router=new Router();
 router.get('/index.html', async ctx => {
-    ctx.body='test'
+    ctx.body='test1'
 })
 module.exports=router.routes();
